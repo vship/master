e528178ab235dda51b20110776a7bd358fc59654
Vladimir <57663745+vship@users.noreply.github.com>
2019-11-18 15:21:01 +0400

    Update index.php

--- docker/php/index.php
+++ docker/php/index.php
@@ -1,5 +1,5 @@
 <?php
-$host = 'db';  //the name of the mysql service inside the docker file.
+$host = 'sql';  //the name of the mysql service inside the docker file.
 $user = 'devuser';
 $password = 'devpass';
 $db = 'test_db';
